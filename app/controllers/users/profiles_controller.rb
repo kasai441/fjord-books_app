4587883bb2f6f49a5e13a7c46a5066666b09af2c
kasai441 <kasai441441@gmail.com>
2021-02-24 11:07:34 +0900

    ユーザshowページ追加、サンプルユーザをseeds.rbで生成

---
+++ app/controllers/users/profiles_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Users::ProfilesController < ApplicationController
+  # GET /users
+  def index
+    @users = User.order(:id).page(params[:page]).per(10)
+  end
+
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+end
