01023834b8a76cb29d8659c4a8a46ee9e903dc8e
kasai441 <kasai441441@gmail.com>
2021-03-08 14:00:34 +0900

    Rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,8 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.github_data"] && session["devise.github_data"]["info"]
-        user.email = data["email"] if user.email.blank?
+      if (data = session['devise.github_data']) && session['devise.github_data']['info'] && user.email.blank?
+        user.email = data['email']
       end
     end
   end
