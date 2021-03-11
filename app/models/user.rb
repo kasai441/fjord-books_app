4ac86a98e2dd5f2559d77e34c086c750b24d4013
kasai441 <kasai441441@gmail.com>
2021-03-11 15:00:30 +0900

    フォロー機能実装する

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@
 
 class User < ApplicationRecord
   has_one_attached :avatar
+  has_many :user_follows, inverse_of: :follower, foreign_key: 'follower_id', dependent: :destroy
+  has_many :followings, through: :user_follows
+  has_many :reverse_user_follows, inverse_of: :following, foreign_key: 'following_id',
+                                  class_name: 'UserFollow',
+                                  dependent: :destroy
+  has_many :followers, through: :reverse_user_follows
 
   validates :email, presence: true, uniqueness: true
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
@@ -16,5 +22,17 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
+  end
+
+  def following?(other_user)
+    user_follows.find_by(following_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    user_follows.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    user_follows.find_by(following_id: other_user.id).destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'user_follows/destroy'
+  get 'user_follows/create'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
   root to: 'books#index'
   resources :books
-  resources :users, only: %i(index show)
+  resources :users, only: %i(index show) do
+    member do
+      get :followings, :followers
+    end
+  end
+  resources :user_follows, only: [:create, :destroy]
 end
