bd2fa19eee22df43de08ca0d9d8cb057991a75dc
kasai441 <kasai441441@gmail.com>
2021-03-15 15:04:06 +0900

    omniauthのmigrationが抜けていたので、復帰させた

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_052416) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -79,11 +79,11 @@ ActiveRecord::Schema.define(version: 2021_03_12_052416) do
     t.string "postal_code"
     t.string "address"
     t.text "self_introduction"
-    t.string "uid"
     t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
