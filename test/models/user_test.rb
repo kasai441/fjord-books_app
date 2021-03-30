76ef36a5167c11498839525ee3a92f15c2969d55
kasai441 <kasai441441@gmail.com>
2021-03-30 15:45:24 +0900

    userモデル#follow, #unfollowテスト追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,4 +12,21 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email: 'foo@example.com', name: 'Foo Bar')
     assert_equal 'Foo Bar', user.name_or_email
   end
+
+  test '#follow should add relationships between users' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+  end
+
+  test '#unfollow should remove relationships between users' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    me.follow(she)
+    assert me.following?(she)
+    me.unfollow(she)
+    assert_not me.following?(she)
+  end
 end
