b7ff882a75a2c33cf81738af55e5df6b73e4f13e
kasai441 <kasai441441@gmail.com>
2021-03-30 16:39:30 +0900

    fixture導入 userモデル#following? #followed_by?テスト追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,29 +4,46 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test '#name_or_email should return email unless name presents' do
-    user = User.new(email: 'foo@example.com', name: '')
-    assert_equal 'foo@example.com', user.name_or_email
+    one = users(:one)
+    assert_equal 'one@example.com', one.name_or_email
   end
 
   test '#name_or_email should return name if name presents' do
-    user = User.new(email: 'foo@example.com', name: 'Foo Bar')
-    assert_equal 'Foo Bar', user.name_or_email
+    one = users(:one)
+    one.name = 'One'
+    assert_equal 'One', one.name_or_email
   end
 
   test '#follow should add relationships between users' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+    one = users(:one)
+    two = users(:two)
+    assert_not one.following?(two)
+    one.follow(two)
+    assert one.following?(two)
   end
 
   test '#unfollow should remove relationships between users' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-    me.follow(she)
-    assert me.following?(she)
-    me.unfollow(she)
-    assert_not me.following?(she)
+    one = users(:one)
+    two = users(:two)
+    one.follow(two)
+    assert one.following?(two)
+    one.unfollow(two)
+    assert_not one.following?(two)
+  end
+
+  test '#following? should return true if one user follows another' do
+    one = users(:one)
+    two = users(:two)
+    assert_not one.following?(two)
+    one.follow(two)
+    assert one.following?(two)
+  end
+
+  test '#followed_by? should return true if one user is followed by another' do
+    one = users(:one)
+    two = users(:two)
+    assert_not one.followed_by?(two)
+    two.follow(one)
+    assert one.followed_by?(two)
   end
 end
