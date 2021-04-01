b3aa2fa9c9ea385a80f70a0cd1c59c2a09a85003
kasai441 <kasai441441@gmail.com>
2021-04-01 11:55:58 +0900

    レポートの変数修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,20 +4,20 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   setup do
-    @report_1 = reports(:report_1)
+    @report = reports(:default_report)
   end
   test '#editable? should return true if target user equals report user' do
     target_user = users(:alice)
-    assert @report_1.editable?(target_user)
+    assert @report.editable?(target_user)
   end
 
   test '#editable? should return false if target user differs report user' do
     target_user = users(:bob)
-    assert_not @report_1.editable?(target_user)
+    assert_not @report.editable?(target_user)
   end
 
   test '#created_on should return created date' do
-    @report_1.created_at = Date.new(2021, 3, 31)
-    assert_equal Date.new(2021, 3, 31), @report_1.created_on
+    @report.created_at = Date.new(2021, 3, 31)
+    assert_equal Date.new(2021, 3, 31), @report.created_on
   end
 end
