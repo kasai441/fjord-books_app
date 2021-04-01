bc348ea7adbd01912fd42d65c7e79191f6e1998d
kasai441 <kasai441441@gmail.com>
2021-04-01 10:15:47 +0900

    fixture名を具体的に変更、各テストに共通処理を追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,21 +3,21 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+  setup do
+    @report_1 = reports(:report_1)
+  end
   test '#editable? should return true if target user equals report user' do
-    one = reports(:one)
-    target_user = users(:one)
-    assert one.editable?(target_user)
+    target_user = users(:alice)
+    assert @report_1.editable?(target_user)
   end
 
   test '#editable? should return false if target user differs report user' do
-    one = reports(:one)
-    target_user = users(:two)
-    assert_not one.editable?(target_user)
+    target_user = users(:bob)
+    assert_not @report_1.editable?(target_user)
   end
 
   test '#created_on should return created date' do
-    one = reports(:one)
-    one.created_at = Date.new(2021, 3, 31)
-    assert_equal Date.new(2021, 3, 31), one.created_on
+    @report_1.created_at = Date.new(2021, 3, 31)
+    assert_equal Date.new(2021, 3, 31), @report_1.created_on
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,48 +3,43 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  setup do
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
   test '#name_or_email should return email unless name presents' do
-    one = users(:one)
-    assert_equal 'one@example.com', one.name_or_email
+    assert_equal 'alice@example.com', @alice.name_or_email
   end
 
   test '#name_or_email should return name if name presents' do
-    one = users(:one)
-    one.name = 'One'
-    assert_equal 'One', one.name_or_email
+    @alice.name = 'Alice'
+    assert_equal 'Alice', @alice.name_or_email
   end
 
   test '#follow should add relationships between users' do
-    one = users(:one)
-    two = users(:two)
-    assert_not one.following?(two)
-    one.follow(two)
-    assert one.following?(two)
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
   end
 
   test '#unfollow should remove relationships between users' do
-    one = users(:one)
-    two = users(:two)
-    one.follow(two)
-    assert one.following?(two)
-    one.unfollow(two)
-    assert_not one.following?(two)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+    @alice.unfollow(@bob)
+    assert_not @alice.following?(@bob)
   end
 
   test '#following? should return true if one user follows another' do
-    one = users(:one)
-    two = users(:two)
-    assert_not one.following?(two)
-    one.follow(two)
-    assert one.following?(two)
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
   end
 
   test '#followed_by? should return true if one user is followed by another' do
-    one = users(:one)
-    two = users(:two)
-    assert_not one.followed_by?(two)
-    two.follow(one)
-    assert one.followed_by?(two)
+    assert_not @alice.followed_by?(@bob)
+    @bob.follow(@alice)
+    assert @alice.followed_by?(@bob)
   end
 
   test '#from_omniauth should return existing user if it exists' do
